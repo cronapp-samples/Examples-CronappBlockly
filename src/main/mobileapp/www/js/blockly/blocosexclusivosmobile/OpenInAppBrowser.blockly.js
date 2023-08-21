e4faf1ef4d8a35edd6f051d848d3a0cf06274a52
Laila Maria Vieira Souza <laila.souza@cronapp.io>
2023-08-21 18:28:57 +0000

    Inserção de novo form e bloco.

---
+++ src/main/mobileapp/www/js/blockly/blocosexclusivosmobile/OpenInAppBrowser.blockly.js
@@ -0,0 +1,22 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.blocosexclusivosmobile = window.blockly.js.blockly.blocosexclusivosmobile || {};
+window.blockly.js.blockly.blocosexclusivosmobile.OpenInAppBrowser = window.blockly.js.blockly.blocosexclusivosmobile.OpenInAppBrowser || {};
+
+/**
+ * @function Example
+ *
+ *
+ *
+ *
+ * @author Laila Maria Vieira Souza
+ * @since 21/08/2023, 15:21:58
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.OpenInAppBrowser.ExampleArgs = [];
+window.blockly.js.blockly.blocosexclusivosmobile.OpenInAppBrowser.Example = async function() {
+
+  //
+  this.cronapi.cordova.database.openInAppBrowser('https://www.cronapp.io/');
+}
