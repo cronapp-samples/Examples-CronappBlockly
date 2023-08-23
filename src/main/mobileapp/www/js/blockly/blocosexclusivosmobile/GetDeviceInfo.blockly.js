e9a9c7f7d1960bfcd300452925e6918f0e877587
Fábio Duarte Freitas <fabio.freitas@cronapp.io>
2023-08-23 12:50:05 +0000

    Adicionei mais exemplos ao bloco get device info e mais botões na página get device info

--- src/main/mobileapp/www/js/blockly/blocosexclusivosmobile/GetDeviceInfo.blockly.js
+++ src/main/mobileapp/www/js/blockly/blocosexclusivosmobile/GetDeviceInfo.blockly.js
@@ -11,14 +11,128 @@ window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo = window.blockly.
  *
  *
  * @author Fábio Duarte Freitas
- * @since 22/08/2023, 16:34:00
+ * @since 23/08/2023, 08:56:31
  *
  */
 window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.ExampleArgs = [];
 window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example = async function() {
  var obterInformacao;
   //
+  obterInformacao = this.cronapi.cordova.device.getDeviceInfo('uuid');
+  //
+  this.cronapi.screen.notify('success',obterInformacao);
+}
+
+/**
+ * @function Example2
+ *
+ *
+ *
+ *
+ * @author Fábio Duarte Freitas
+ * @since 23/08/2023, 08:56:31
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example2Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example2 = async function() {
+ var obterInformacao;
+  //
   obterInformacao = this.cronapi.cordova.device.getDeviceInfo('model');
+  //
+  this.cronapi.screen.notify('success',obterInformacao);
+}
+
+/**
+ * @function Example3
+ *
+ *
+ *
+ *
+ * @author Fábio Duarte Freitas
+ * @since 23/08/2023, 08:56:31
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example3Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example3 = async function() {
+ var obterInformacao;
+  //
+  obterInformacao = this.cronapi.cordova.device.getDeviceInfo('platform');
+  //
+  this.cronapi.screen.notify('success',obterInformacao);
+}
+
+/**
+ * @function Example4
+ *
+ *
+ *
+ *
+ * @author Fábio Duarte Freitas
+ * @since 23/08/2023, 08:56:31
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example4Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example4 = async function() {
+ var obterInformacao;
+  //
+  obterInformacao = this.cronapi.cordova.device.getDeviceInfo('version');
+  //
+  this.cronapi.screen.notify('success',obterInformacao);
+}
+
+/**
+ * @function Example5
+ *
+ *
+ *
+ *
+ * @author Fábio Duarte Freitas
+ * @since 23/08/2023, 08:56:31
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example5Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example5 = async function() {
+ var obterInformacao;
+  //
+  obterInformacao = this.cronapi.cordova.device.getDeviceInfo('manufacturer');
+  //
+  this.cronapi.screen.notify('success',obterInformacao);
+}
+
+/**
+ * @function Example6
+ *
+ *
+ *
+ *
+ * @author Fábio Duarte Freitas
+ * @since 23/08/2023, 08:56:31
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example6Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example6 = async function() {
+ var obterInformacao;
+  //
+  obterInformacao = this.cronapi.cordova.device.getDeviceInfo('isVirtual');
+  //
+  this.cronapi.screen.notify('success',obterInformacao);
+}
+
+/**
+ * @function Example7
+ *
+ *
+ *
+ *
+ * @author Fábio Duarte Freitas
+ * @since 23/08/2023, 08:56:31
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example7Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.GetDeviceInfo.Example7 = async function() {
+ var obterInformacao;
+  //
+  obterInformacao = this.cronapi.cordova.device.getDeviceInfo('serial');
   //
   this.cronapi.screen.notify('success',obterInformacao);
 }
