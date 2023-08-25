0b113fe365717254133c400bf03bdefafc4b8f76
Laila Maria Vieira Souza <laila.souza@cronapp.io>
2023-08-25 20:01:47 +0000

    Atualizações.

--- src/main/mobileapp/www/js/blockly/blocosexclusivosmobile/VerifyConnection.blockly.js
+++ src/main/mobileapp/www/js/blockly/blocosexclusivosmobile/VerifyConnection.blockly.js
@@ -11,14 +11,52 @@ window.blockly.js.blockly.blocosexclusivosmobile.VerifyConnection = window.block
  *
  *
  * @author Laila Maria Vieira Souza
- * @since 25/08/2023, 15:55:12
+ * @since 25/08/2023, 16:59:30
  *
  */
 window.blockly.js.blockly.blocosexclusivosmobile.VerifyConnection.ExampleArgs = [];
 window.blockly.js.blockly.blocosexclusivosmobile.VerifyConnection.Example = async function() {
  var status;
   //
+  status2 = this.cronapi.cordova.connection.verifyConnection('Connection.WIFI');
+  //
+  this.cronapi.screen.notify('success',String('Status da Rede: ') + String(status2));
+}
+
+/**
+ * @function Example2
+ *
+ *
+ *
+ *
+ * @author Laila Maria Vieira Souza
+ * @since 25/08/2023, 16:59:30
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.VerifyConnection.Example2Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.VerifyConnection.Example2 = async function() {
+ var status;
+  //
   status2 = this.cronapi.cordova.connection.verifyConnection('Connection.CELL_4G');
+  //
+  this.cronapi.screen.notify('success',String('Status da Rede: ') + String(status2));
+}
+
+/**
+ * @function Example3
+ *
+ *
+ *
+ *
+ * @author Laila Maria Vieira Souza
+ * @since 25/08/2023, 16:59:30
+ *
+ */
+window.blockly.js.blockly.blocosexclusivosmobile.VerifyConnection.Example3Args = [];
+window.blockly.js.blockly.blocosexclusivosmobile.VerifyConnection.Example3 = async function() {
+ var status;
+  //
+  status2 = this.cronapi.cordova.connection.verifyConnection('Connection.CELL');
   //
   this.cronapi.screen.notify('success',String('Status da Rede: ') + String(status2));
 }
--- src/main/mobileapp/www/js/hostApp.js
+++ src/main/mobileapp/www/js/hostApp.js
@@ -1,1 +1,1 @@
-window.hostApp = 'https://app-30-250-49212.ide.cronapp.io/';
+window.hostApp = 'https://app-8-231-49360.ide.cronapp.io/';
